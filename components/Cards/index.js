49606607bfcc983d35d591ef24d750dd048b6310
LyfeTyme <48641165+LyfeTyme@users.noreply.github.com>
2019-07-21 21:57:30 -0700

    mvp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,85 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+let getOne = 'https://lambda-times-backend.herokuapp.com/articles';
+
+function getArticleData (getOne) {
+
+    axios.get(getOne)
+    .then(response => {
+
+
+        let articleCont = [];
+        articleCont = response["data"]["articles"];
+
+        let articleTitle = "javascript";
+
+        createArt(articleCont, articleTitle);
+
+        // 
+
+        articleTitle = "bootstrap";
+        createArt(articleCont, articleTitle);
+
+        // 
+       articleTitle = "technology";
+        createArt(articleCont, articleTitle);
+        // 
+        articleTitle = "jquery";
+        createArt(articleCont, articleTitle);
+        // 
+         articleTitle = "node.js";
+        createArt(articleCont, articleTitle);
+        
+    })
+
+    .catch (error => {
+        console.log(error);
+    })
+};
+
+function createArt(articleCont,articleTitle) {
+
+let y = "headline";  
+let z = "authorName";
+let p = "authorPhoto";
+
+ for(i in articleCont[articleTitle]) {       
+
+    let mainContainerDiv = document.getElementsByClassName("cards-container")[0];
+    let newDiv0 = document.createElement('div');
+    newDiv0.classList.add('card'); 
+
+     let newDiv1 = document.createElement('div');
+    newDiv1.classList.add('headline'); 
+    newDiv1.innerText = articleCont[articleTitle][i][y];
+
+     let newDiv2 = document.createElement('div');
+    newDiv2.classList.add('author'); 
+    newDiv2.innerText = articleCont[articleTitle][i][z];
+
+     let newDiv3 = document.createElement('div');
+    newDiv3.classList.add('img-container');
+
+    let newSpan = document.createElement('span');
+
+    let newImg =  document.createElement('img');
+    newImg.src = articleCont[articleTitle][i][p];    
+
+
+    newDiv3.appendChild(newImg);
+    newDiv2.appendChild(newDiv3);
+    newDiv2.appendChild(newSpan);
+    newDiv1.appendChild(newDiv2);
+    newDiv0.appendChild(newDiv1);
+    mainContainerDiv.appendChild(newDiv0);
+
+    }
+
+
+}
+
+
+getArticleData(getOne);
--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,32 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
+function Header() {
 
-function Header() {}
+
+   let headerContainer = document.getElementsByClassName("header-container")[0];
+
+   let newDiv = document.createElement('div');
+   newDiv.classList.add('header'); 
+
+   let newSpan0 = document.createElement('span');
+   newSpan0.classList.add('date');
+   newSpan0.innerText = 'July 21, 2019'; 
+
+   let newH1 = document.createElement('h1');
+   newH1.innerText = 'Lambda Times';
+
+   let newSpan1 = document.createElement('span');
+   newSpan1.classList.add('temp'); 
+   newSpan1.innerText = '140°';
+
+   newDiv.appendChild(newSpan0);
+   newDiv.appendChild(newH1);
+   newDiv.appendChild(newSpan1);
+
+   headerContainer.appendChild(newDiv);
+
+}
+
+
+Header();
